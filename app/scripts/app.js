b12acd12faa20bb6a34eb97cc1d004efaae4228e
Wajid Kagzi <wjkagzi@gmail.com>
2015-06-19 17:45:08 -0400

    Initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mytodoApp
+ * @description
+ * # mytodoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mytodoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

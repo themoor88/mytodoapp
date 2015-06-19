43970e9a51bcf4a0b44c1dedea60b00cc124801e
Wajid Kagzi <wjkagzi@gmail.com>
2015-06-19 17:51:36 -0400

    Added todos model.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,6 @@
  * Controller of the mytodoApp
  */
 angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', function($scope){
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+  }]);
